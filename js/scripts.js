533188527f165ee42198e994c60bd2b0ee06426b
Brianna Popson <bpopson@gmail.com>
2016-01-29 11:19:37 -0800

    finally create second passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 function PizzaMaker() {
-  this.pieSize = ["not to share", "to share", "study group", "drunk party"];
+  this.basePrice = this.crustSize();
+  this.basicTops = [];
+  this.specialTops = [];
 }
 
 PizzaMaker.prototype.crustSize = function(size) {
-  if (size === "not to share") { //not to share is a personal sized pizza or a small
-  return (7);
-} else if (size === "to share"){ //share is a medium sized pizza
-  return (9);
-} else if (size === "study group"){ //study group is a large pizza
+  if (size === "notToShare") { //not to share is a personal sized pizza or a small
+    return (7);
+  } else if (size === "toShare"){ //share is a medium sized pizza
+    return (9);
+  } else if (size === "studyGroup"){ //study group is a large pizza
     return (11);
-  } else if (size === "drunk party"){ //drunk party is an extra large pizza
+  } else if (size === "drunkParty"){ //drunk party is an extra large pizza
     return (12);
+  }
+}
+
+PizzaMaker.prototype.basics = function(tops) {
+  var basePrice = this.crustSize("drunkParty");
+  console.log(basePrice);
+  if (tops.length <= 3) {
+  return (basePrice);
   }
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,11 @@
 describe('PizzaMaker', function(){
-  it("will allow user to select a pizza size and apply a base cost", function(){
+  it("will allow user to select a pizza size and establish a base cost", function(){
     var testPizza = new PizzaMaker();
-    expect(testPizza.crustSize("drunk party")).to.equal(12);
+    expect(testPizza.crustSize("drunkParty")).to.equal(12);
   }); //worked with all sizes of pizza
+
+  it("will allow user to add certain toppings with no additional charge", function(){
+    var testPizza = new PizzaMaker();
+    expect(testPizza.basics(["pepperoni", "mushrooms", "onion"])).to.equal(12);
+  });
 });
